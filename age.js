de27e8a802d2d037007bca73e0ea6feddd406854
Igor V. Kruglenko <igor.kruglenko@gmail.com>
2017-08-29 13:33:26 +0300

    Мелкие исправления в соответствии с JSLint

--- age.js
+++ age.js
@@ -7,15 +7,14 @@ function age() {
     currentYear = 2017;
     uAge = document.getElementById('Age').value;
 
-    if ((uAge.length > 2 && uAge.length !== 4) || uAge.length === 0 || uAge.parseInt > currentYear || typeof (parseInt(uAge)) === 'undefined') {
+    if ((uAge.length > 2 && uAge.length !== 4) || uAge.length === 0 || parseInt(uAge, 0) > currentYear || parseInt(uAge, 0) === 'undefined') {
         result = "Что-то пошло не так! Проверьте заполненные поля.";
         document.getElementById("outAge").innerHTML = result;
         document.getElementById('Age').className = 'uk-input uk-form-danger';
         return;
     }
     document.getElementById('Age').className = 'uk-input uk-form-success';
-    console.log();
-    if (((uAge.length <= 2 && parseInt(uAge)) >= 1 && (parseInt(uAge) < 16)) || (uAge.length === 4 && (currentYear - parseInt(uAge) <= 16))) {
+    if (((uAge.length <= 2 && parseInt(uAge, 10)) >= 1 && (parseInt(uAge, 10) < 16)) || (uAge.length === 4 && (currentYear - parseInt(uAge, 10) <= 16))) {
         result = 'Вы еще молоды';
         document.getElementById('Age').className = 'uk-input uk-form-danger';
     } else {
